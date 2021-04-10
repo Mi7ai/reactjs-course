309c8dcdf441d0540e5e17a7333bc5fc2eec1e81
mi7ai <hottmayer@gmail.com>
2021-04-10 20:26:37 +0200

    nested components

--- t1/src/index.js
+++ t1/src/index.js
@@ -1,10 +1,35 @@
-import React from "react";
-import ReactDom from "react-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
 
-// must capitablize 1st leter
+// must capitalize 1st leter
 // this is a component
+// enclose within parentes
 function Greeting() {
-  return <h1>This is Jognny</h1>;
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById("root"));
+// nested component
+
+const Person = () => {
+  return (
+    <div>
+      <h2>Mihai</h2>
+    </div>
+  );
+};
+
+// nested component
+const Message = () => {
+  return (
+    <div>
+      <h3>This is my message</h3>
+    </div>
+  );
+};
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
