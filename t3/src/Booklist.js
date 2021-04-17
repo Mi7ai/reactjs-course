cadaf9dcd957a3709e735eec8700e5343eef6509
mi7ai <hottmayer@gmail.com>
2021-04-17 15:51:02 +0200

    elements with array

--- t3/src/Booklist.js
+++ t3/src/Booklist.js
@@ -1,23 +1,25 @@
 import './index.css';
+const books = [
+  {
+    title: 'Travian title',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fblog.travian.com%2Fwp-content%2Fuploads%2F2015%2F06%2Ftravian-kingdoms-scout.jpg&f=1&nofb=1',
+    author: 'Mihai',
+  },
 
-const book2 = {
-  title: 'Travian title 2',
-  image:
-    'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fblog.travian.com%2Fwp-content%2Fuploads%2F2015%2F06%2Ftravian-kingdoms-scout.jpg&f=1&nofb=1',
-  author: 'Dixy',
-};
-
+  {
+    title: 'Travian title 2',
+    image:
+      'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fblog.travian.com%2Fwp-content%2Fuploads%2F2015%2F06%2Ftravian-kingdoms-scout.jpg&f=1&nofb=1',
+    author: 'Dixy',
+  },
+];
 function Booklist() {
   return (
     <div className="booklist">
-      {/* ONE WAY OF DOING IT */}
-      <Book
-        title="Travian title"
-        image="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fblog.travian.com%2Fwp-content%2Fuploads%2F2015%2F06%2Ftravian-kingdoms-scout.jpg&f=1&nofb=1"
-        author="Mihai"
-      />
-      {/* ONE WAY OF DOING IT */}
-      <Book title={book2.title} image={book2.image} author={book2.author} />
+      {books.map((book) => {
+        return <Book book={book} />;
+      })}
     </div>
   );
 }
@@ -25,9 +27,9 @@ function Booklist() {
 const Book = (props) => {
   return (
     <article className="book">
-      <img src={props.image} alt="" />
-      <h1>{props.title}</h1>
-      <h2>{props.author}</h2>
+      <img src={props.book.image} alt="" />
+      <h1>{props.book.title}</h1>
+      <h2>{props.book.author}</h2>
     </article>
   );
 };
